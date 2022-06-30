c36b84df2959f2fbd7d832cd56d9e2ee64d9a267
Navisino <navisinokate@gmail.com>
2022-06-30 12:31:36 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
  * main - main function in the task
  *
  *
- *Return: return 1
+ *Return: 1
  */
 int main(void)
 {
-		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		return (1);
 }
 
 
