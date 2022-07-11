708815ef175151fa9bb9850160c6ae3794c59f0a
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-07-11 15:09:26 +0300

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,20 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * *_isupper - function that checks for uppercase character
- * *
- * *@c:  input interger
- * *Return: Retuns 1 for uppercase and 0 for anything else
- * */
+*_isupper - function that checks for uppercase character
+*
+*@c:  input interger
+*Return: Retuns 1 for uppercase and 0 for anything else
+*/
 int _isupper(int c)
 {
 
-		if (c >= 'A' && c <= 'Z')
-				{
-							return (1);
-								}
-			else
-					{
-								return (0);
-									}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
