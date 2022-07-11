af90c167a183c5e365d05e5b619740bdb5d19f23
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-07-11 15:08:27 +0300

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isdigit - checks if parameter is an uppercase character.
+*
+* @c: input a number
+*
+* Return: 1 if c is a digit, 0 in any other case.
+*/
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
