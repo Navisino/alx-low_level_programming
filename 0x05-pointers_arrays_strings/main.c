5b57b2c5f005e5738bb2a780a9b20e9b6c79df82
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-07-12 14:15:07 +0300

    main.c

---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+
+    char *s;
+
+
+
+    s = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+
+    print_rev(s);
+
+    return (0);
+
+}
