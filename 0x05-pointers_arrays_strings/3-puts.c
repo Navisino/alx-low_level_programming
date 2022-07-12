3f14086e74fc230e2bf3bb260e6b949f3c77e0da
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-07-12 14:18:44 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+* _puts - prints a string
+* @str: string
+* Return: 0
+*/
+
+void _puts(char *str)
+
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
