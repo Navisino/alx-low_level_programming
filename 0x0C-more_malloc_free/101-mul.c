fec79130c2dcea52d9e01a9eabdf3ef8699a3fc9
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-01 17:55:37 +0300

    Don't give up on infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,97 +1,27 @@
-#include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
 
-#include <string.h>
-
-
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * main - Check the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: Nothing
  */
-
-void simple_print_buffer(char *buffer, unsigned int size)
-
+int main(int argc, char **argv)
 {
+	int num1, num2, result;
 
-    unsigned int i;
-
-
-
-    i = 0;
-
-    while (i < size)
-
-    {
-
-        if (i % 10)
-
-        {
-
-            printf(" ");
-
-        }
-
-        if (!(i % 10) && i)
-
-        {
-
-            printf("\n");
-
-        }
-
-        printf("0x%02x", buffer[i]);
-
-        i++;
-
-    }
-
-    printf("\n");
-
-}
-
-
-
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-
-int main(void)
-
-{
-
-    char *p;
-
-    int i;
-
-
-
-    p = malloc(sizeof(char) * 10);
-
-    p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-
-    i = 0;
-
-    while (i < 98)
-
-    {
-
-        p[i++] = 98;
-
-    }
-
-    simple_print_buffer(p, 98);
-
-    free(p);
-
-    return (0);
-
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
