00a5ab8fbac624f0122e9cfb6177f621b1ccfd46
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-03 14:13:32 +0300

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,11 @@
 #include "function_pointers.h"
-
 /**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
+ * print_name - Write a function that prints a name
+ * @name: name of the person
+ * @f: paramter for the function pointer
+ * Return: Nothing.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	(*f)(name);
 }
