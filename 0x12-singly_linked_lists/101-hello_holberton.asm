dc58047ce2099f175288e4eb28bc9796a6129ff7
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-11 19:20:47 +0300

    Hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,18 @@
-#include <stdio.h>
+	extern printf
 
-/**
- * 64-bit program in assembly that prints Hello, Holberton.
- */
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
 
-int main(void)
-{
-printf("Hello, Holberton\n");
-return 0;
-}
+	section .text
+
+	global main
+main:
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
