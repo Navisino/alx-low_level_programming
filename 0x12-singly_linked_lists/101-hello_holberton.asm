fa5db3eb55b742075153b09d5706cc3205c80c14
Kate Navisino <107048843+Navisino@users.noreply.github.com>
2022-08-11 17:58:21 +0300

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,6 +5,5 @@
  */
 void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("Hello, Holberton\n");
 }
