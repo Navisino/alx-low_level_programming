5ee5cad81f3bcbb3f2e48fc1abd0221715864b3a
Navisino <navisinokate@gmail.com>
2022-08-22 11:55:30 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
 		rstatus = write(fd, text_content, i);
-		if (rstatus == -1)								return (-1);
+		if (rstatus == -1)								
+			return (-1);
 	}
 
 	close(fd);
